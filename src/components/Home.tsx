b82c7658c170342754eac1063e411cc1abbc349c
s2s2hyun <s2s2hyun0601@gmail.com>
2023-12-22 19:22:23 +0900

    gps location 설명

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+interface LocationProps {
+  location: {
+    latitude: number;
+    longitude: number;
+  };
+}
 
 const Home = () => {
-  return <div>Home</div>;
+  const [location, setLocation] = useState<LocationProps["location"]>({
+    latitude: 0,
+    longitude: 0,
+  });
+
+  useEffect(() => {
+    let watchId: number | null = null;
+    if (navigator.geolocation) {
+      watchId = navigator.geolocation.watchPosition(
+        (position) => {
+          setLocation({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+          });
+        },
+        (error) => {
+          console.log(error);
+        },
+        {
+          enableHighAccuracy: true,
+          timeout: 5000,
+          maximumAge: 1000,
+        }
+      );
+    } else {
+      console.log("Geolocation is not supported by this browser.");
+    }
+
+    return () => {
+      if (watchId) navigator.geolocation.clearWatch(watchId);
+    };
+  }, []);
+
+  return (
+    <div>
+      <p>{location.latitude}</p>
+      <p>{location.longitude}</p>
+    </div>
+  );
 };
 
 export default Home;
