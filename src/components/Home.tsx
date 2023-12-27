45c16433bb2fdeadecf491b1e8c683d2173cf9c1
s2s2hyun <s2s2hyun0601@gmail.com>
2023-12-27 20:41:12 +0900

    data.kr api 확인완료 , 3d model 마스크

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -9,14 +9,37 @@ import axios from "axios";
 //   stationName: string;
 // }
 
+interface Coordinates {
+  x: number;
+  y: number;
+}
+
 const Home = () => {
   const public_key = import.meta.env.VITE_REALTIME_AIR_KEY;
   const kakao_key = import.meta.env.VITE_APP_KAKAO_KEY;
-  const kakao_js_key = import.meta.env.VITE_APP_KAKAO_JS_KEY;
+  // const kakao_js_key = import.meta.env.VITE_APP_KAKAO_JS_KEY;
   const location = useGeolocation();
   const [address, setAddress] = useState<string>("");
-  const [changeCoord, setChangeCoord] = useState({ x: 0, y: 0 });
-  const [center, setCenter] = useState("");
+  const [changeCoord, setChangeCoord] = useState<Coordinates>({ x: 0, y: 0 });
+  const [isCoordValid, setIsCoordValid] = useState<boolean>(false);
+  const [center, setCenter] = useState<string>("");
+  // 대기오염
+  const [dusty, setDusty] = useState<string>("");
+  const [grade, setGrade] = useState<string>("");
+  const [oz, setOz] = useState<string>(""); // 오존
+  const [ozGrade, setOzGrade] = useState<string>(""); // 오존등급
+
+  const [co, setCo] = useState<string>(""); // 일산화탄소
+  const [coGrade, setCoGrade] = useState(""); // 일산화탄소등급
+
+  const [so, setSo] = useState<string>(""); // 아황산가스
+  const [soGrade, setSoGrade] = useState<string>(""); // 아황산가스등급
+
+  const [no, setNo] = useState<string>(""); // 이산화질소
+  const [noGrade, setNoGrade] = useState<string>(""); // 이산화질소등급
+
+  const [khai, setKhai] = useState<string>(""); // 통합대기
+  const [khaiGrade, setKhaiGrade] = useState<string>(""); // 통합대기등급
 
   const fetchAddress = useCallback(
     async (lat: number, lng: number, key: string) => {
@@ -30,7 +53,7 @@ const Home = () => {
           }
         );
         const addressData = response.data.documents?.[0]?.address;
-        console.log(response);
+        // console.log(response);
         if (addressData) {
           setAddress(
             `${addressData.region_1depth_name} ${addressData.region_2depth_name} ${addressData.region_3depth_name}`
@@ -46,67 +69,153 @@ const Home = () => {
   );
 
   useEffect(() => {
-    if (location.coordinates && !address) {
+    if (
+      location.coordinates &&
+      location.coordinates.lat &&
+      location.coordinates.lng
+    ) {
       fetchAddress(
         location.coordinates.lat,
         location.coordinates.lng,
         kakao_key
       );
+
+      axios
+        .get(
+          `https://dapi.kakao.com/v2/local/geo/transcoord.json?x=${location.coordinates.lng}&y=${location.coordinates.lat}&input_coord=WGS84&output_coord=TM`,
+          {
+            headers: {
+              Authorization: "KakaoAK " + kakao_key,
+            },
+          }
+        )
+        .then((res) => {
+          const coordinatesData = res.data.documents[0];
+          if (
+            typeof coordinatesData.x === "number" &&
+            typeof coordinatesData.y === "number"
+          ) {
+            setChangeCoord({ x: coordinatesData.x, y: coordinatesData.y });
+            setIsCoordValid(true);
+          } else {
+            console.log("Invalid coordinates data");
+            // 필요에 따라 여기에 기본값을 설정하거나 다른 처리를 할 수 있습니다.
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     }
-  }, [location, kakao_key, address, fetchAddress]);
-
-  // console.log(location.coordinates.lat, typeof location.coordinates.lat);
-
-  const response1 = axios
-    .get(
-      `https://dapi.kakao.com/v2/local/geo/transcoord.json?x=${location.coordinates.lng}&y=${location.coordinates.lat}&input_coord=WGS84&output_coord=TM`,
-      {
-        headers: {
-          Authorization: "KakaoAK " + kakao_key,
-        },
-      }
-    )
-    .then((res) => {
-      // console.log(res.data);
-      const coordinatesData = res.data.documents[0];
-      setChangeCoord({ x: coordinatesData.x, y: coordinatesData.y });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  }, [location, kakao_key, fetchAddress]);
 
   console.log(changeCoord, " changeCoord");
 
-  // const reponse2 = axios
-  //   .get(
-  //     `http://apis.data.go.kr/B552584/MsrstnInfoInqireSvc/getNearbyMsrstnList?tmX=${changeCoord.x}&tmY=${changeCoord}&returnType=json&serviceKey=${public_key}`
-  //   )
-  //   .then((res) => {
-  //     const items = res.data.response?.body.items;
-  //     setCenter(items[0]?.stationName);
-  //   })
-  //   .catch((error) => console.log(error));
+  useEffect(() => {
+    if (isCoordValid) {
+      axios
+        .get(
+          `http://apis.data.go.kr/B552584/MsrstnInfoInqireSvc/getNearbyMsrstnList?tmX=${Number(
+            changeCoord.x
+          )}&tmY=${Number(changeCoord.y)}&returnType=json&serviceKey=${String(
+            public_key
+          )}`
+        )
+        .then((res) => {
+          const items = res.data.response?.body.items;
+          if (items && items.length > 0) {
+            setCenter(items[0]?.stationName);
+          } else {
+            console.log("No station data found");
+          }
+        })
+        .catch((err) => console.log(err));
+    }
+  }, [changeCoord, public_key]);
 
-  // const response3 = axios
-  //   .get(
-  //     `https://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?dataTerm=month&pageNo=1&numOfRows=100&returnType=json&stationName=${center[0]}&serviceKey=${public_key}`
-  //   )
-  //   .then((res) => {
-  //     console.log(res.data.response?.body.items);
-  //   })
-  //   .catch((error) => console.log(error));
+  useEffect(() => {
+    if (center) {
+      axios
+        .get(
+          `https://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?dataTerm=month&pageNo=1&numOfRows=100&returnType=json&stationName=${center}&serviceKey=${public_key}`
+        )
+        .then((res) => {
+          console.log(res.data.response?.body.items[0]);
+          setCoGrade(res.data.response?.body.items[0].coGrade);
+          console.log(
+            res.data.response?.body.items[0].coGrade,
+            "coGrade 이산화탄소등급"
+          );
+          setCo(res.data.response?.body.items[0].coValue);
+          console.log(
+            res.data.response?.body.items[0].coValue,
+            "coValue 이산화탄소등급"
+          );
+          setKhaiGrade(res.data.response?.body.items[0].khaiGrade);
+          console.log(
+            res.data.response?.body.items[0].khaiGrade,
+            "khaiGrade 통합대기 등급"
+          );
+          console.log(
+            res.data.response?.body.items[0].khaiValue,
+            "khaiValue 통합대기"
+          );
+          setKhai(res.data.response?.body.items[0].khaiValue);
+          setNoGrade(res.data.response?.body.items[0].no2Grade);
+          console.log(
+            res.data.response?.body.items[0].no2Grade,
+            "no2Grade 이산화질소등급"
+          );
+          console.log(
+            res.data.response?.body.items[0].no2Value,
+            "no2Value 이산화질소"
+          );
+          setNo(res.data.response?.body.items[0].no2Value);
+          setOzGrade(res.data.response?.body.items[0].o3Grade);
+          console.log(
+            res.data.response?.body.items[0].o3Grade,
+            "o3Grade 오존등급"
+          );
+          console.log(res.data.response?.body.items[0].o3Value, "o3Value 오존");
+          setOz(res.data.response?.body.items[0].o3Value);
+          setGrade(res.data.response?.body.items[0].pm10Grade);
+          console.log(
+            res.data.response?.body.items[0].pm10Grade,
+            "pm10Grade 미세먼지등급"
+          );
+          setDusty(res.data.response?.body.items[0].pm10Value);
+          console.log(
+            res.data.response?.body.items[0].pm10Value,
+            "pm10Value 미세먼지"
+          );
+          console.log(
+            res.data.response?.body.items[0].so2Grade,
+            "so2Grade 아황산가스"
+          );
+          setSo(res.data.response?.body.items[0].so2Grade);
+          console.log(
+            res.data.response?.body.items[0].so2Value,
+            "so2Value 아황산가스등급"
+          );
+          setSoGrade(res.data.response?.body.items[0].so2Value);
+        })
+        .catch((error) => console.log(error));
+    }
+  }, [center, public_key]);
 
-  // console.log(reponse1?.data.response.body, " reponse1");
-  // console.log(address, "address 는 현재 ");
+  console.log(center, "center");
 
   return (
     <div>
       {
         <>
-          <p>{kakao_key}</p>
-          <p>{kakao_js_key}</p>
           <p>{location.coordinates.lat}</p>
           <p>{location.coordinates.lng}</p>
+          <p>{changeCoord.x}</p>
+          <p>{changeCoord.y}</p>
+          <p>
+            {center}
+            {"center"}
+          </p>
           <p>{address}</p>
         </>
       }
