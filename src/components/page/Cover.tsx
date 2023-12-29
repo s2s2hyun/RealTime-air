df7ea1c09d80216bf95523441097e3222b815ea2
s2s2hyun <s2s2hyun0601@gmail.com>
2023-12-30 00:37:04 +0900

    fix function

--- src/components/page/Cover.tsx
+++ src/components/page/Cover.tsx
@@ -1,39 +1,40 @@
-import React, { Suspense, useRef } from "react";
+import React, { Suspense } from "react";
 import styled from "@emotion/styled";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Stage } from "@react-three/drei";
 import Mask from "../3dobject/3dMask";
 
-const Cover = () => (
-  /* const ref = useRef();*/ <Wrapper>
-    <Container>
-      <p>공기</p>
-    </Container>
-    <CanvasContainer>
-      <Canvas
-        dpr={[0.5, 0.85]}
-        orthographic
-        camera={{ fov: 75, position: [0, 0, 15] }}
-        style={{ background: "rgba(59,130,246,0)" }}
-      >
-        <Suspense fallback={null}>
-          <Stage preset="soft" intensity={1} environment="warehouse">
-            <Mask />
-          </Stage>
-        </Suspense>
-        <OrbitControls
-          enableZoom={false}
-          enableRotate={false}
-          enablePan={false}
-          autoRotate
-          autoRotateSpeed={1.5}
-        />
-      </Canvas>
-    </CanvasContainer>
-  </Wrapper>
-);
-
-export default Cover;
+export default function Cover() {
+  //   /* const ref = useRef();*/
+  return (
+    <Wrapper>
+      <Container>
+        <p>공기</p>
+      </Container>
+      <CanvasContainer>
+        <Canvas
+          dpr={[0.5, 0.85]}
+          orthographic
+          camera={{ fov: 75, position: [0, 0, 15] }}
+          style={{ background: "rgba(59,130,246,0)" }}
+        >
+          <Suspense fallback={null}>
+            <Stage preset="soft" intensity={1} environment="warehouse">
+              <Mask />
+            </Stage>
+          </Suspense>
+          <OrbitControls
+            enableZoom={false}
+            enableRotate={false}
+            enablePan={false}
+            autoRotate
+            autoRotateSpeed={1.5}
+          />
+        </Canvas>
+      </CanvasContainer>
+    </Wrapper>
+  );
+}
 
 const Wrapper = styled.div`
   width: 100%;
